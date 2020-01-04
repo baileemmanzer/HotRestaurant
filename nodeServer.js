61e664683d9380f1ba5362dec5798554c1e0fa58
Turk <tuckerfiumara@gmail.com>
2020-01-04 14:21:23 -0700

    Needs work

--- nodeServer.js
+++ nodeServer.js
@@ -7,7 +7,7 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var tables = [
+var reservations = [
     {
         name: "Test",
         phoneNumber: "123",
@@ -15,6 +15,8 @@ var tables = [
         id: "please work"
     }
 ]
+
+var waitList = [];
 
 app.get("/", function(req, res) {
     // res.send("Welcome to the Star Wars Page!")
@@ -28,6 +30,45 @@ app.get("/tables", function(req, res) {
 app.get("/reserve", function(req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
+
+app.get("/api/reservations", function(req, res) {
+    return res.json(reservations);
+});
+
+app.get("/api/waitList", function(req, res) {
+    return res.json(reservations);
+});
+
+if (reservations.length < 5) {
+app.post("/api/reservations", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newreservation = req.body;
+  
+    console.log(newreservation);
+  
+    // We then add the json the user sent to the character array
+    reservations.push(newreservation);
+  
+    // We then display the JSON to the users
+    res.json(newreservation);
+  });
+}
+else if (reservations.length > 5) {
+    app.post("/api/waitList", function(req, res) {
+        // req.body hosts is equal to the JSON post sent from the user
+        // This works because of our body parsing middleware
+        var newreservation = req.body;
+      
+        console.log(newreservation);
+      
+        // We then add the json the user sent to the character array
+        waitList.push(newreservation);
+      
+        // We then display the JSON to the users
+        res.json(newreservation);
+      });
+}
 
 
 
