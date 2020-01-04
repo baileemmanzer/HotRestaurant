a9a5b15e651ed71eeb7a260bb001196b6ee60cb5
Turk <tuckerfiumara@gmail.com>
2020-01-04 12:00:08 -0700

    Bug fixes

--- nodeServer.js
+++ nodeServer.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var path = require("path");
+
 var app = express();
 var PORT = 3000;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 var tables = [
     {
         name: "Test",
@@ -12,16 +15,23 @@ var tables = [
         id: "please work"
     }
 ]
+
 app.get("/", function(req, res) {
-    res.send("Welcome to the Star Wars Page!")
-    // res.sendFile(path.join(__dirname, "home.html"));
+    // res.send("Welcome to the Star Wars Page!")
+    res.sendFile(path.join(__dirname, "home.html"));
 });
+
 app.get("/tables", function(req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
 });
+
 app.get("/reserve", function(req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
+
+
+
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
     console.log(__dirname)
